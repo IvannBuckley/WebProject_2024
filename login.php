87de91c73dcbeb9716eb895468d71262a9e3d4a4
Tariq Davy <davytariq@gmail.com>
2024-11-29 20:37:09 -0500

    Added database and database connection

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+include('db.php');
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // check if user is in database
+    $sql = "SELECT * FROM `users` WHERE `email` = :email";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    // If user
+    if ($user) {
+        if (hash('sha256', $password) === $user['password']) {
+            // if password is correct start session
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['firstname'] = $user['firstname'];
+            $_SESSION['role'] = $user['role'];
+            header('Location: #'); 
+            exit;
+        } else {
+            $error = "Invalid password!";
+        }
+    } else {
+        $error = "No user found with that email!";
+    }
+}
+
